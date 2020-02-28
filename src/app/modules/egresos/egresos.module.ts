e575b7083ac972e9494c2506fa77ce886c8bf369
Angel Antezana Flores <ghehahanghel@gmail.com>
2020-02-28 05:54:35 -0400

    Generando modulo Trabajos y egresos

---
+++ src/app/modules/egresos/egresos.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EgresosRoutingModule } from './egresos-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    EgresosRoutingModule
+  ]
+})
+export class EgresosModule { }
---
+++ src/app/modules/trabajos/trabajos.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TrabajosRoutingModule } from './trabajos-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TrabajosRoutingModule
+  ]
+})
+export class TrabajosModule { }
